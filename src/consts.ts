8f845dd71f44e5ca669f13635cc6fdb4b65ae8af
Sergey Hayriyan <sega1ness@gmail.com>
2024-05-24 19:07:06 +0400

    Small enhancements on UI

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Vana Katu';
-export const SITE_DESCRIPTION = 'Vana Katu!';
+export const SITE_DESCRIPTION = 'A surreal story-driven game that uses puzzle-solving, turn based battling, and open world navigation to help Van Cat track down his evil uncle and put an end to his nefarious plans!';
